aeaf983fb90bf57e1da64fc1645966cdc2837858
Mohib <gazi.mohib@brainstation-23.com>
2023-03-01 20:50:53 +0000

    refresh scope

--- src/main/java/com/abc/bank/accountservice/AccountServiceApplication.java
+++ src/main/java/com/abc/bank/accountservice/AccountServiceApplication.java
@@ -3,11 +3,13 @@ package com.abc.bank.accountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.abc.bank.accountservice.controller") })
 @EntityScan("com.abc.bank.accountservice.model")
 @EnableJpaRepositories("com.abc.bank.accountservice.repository")
